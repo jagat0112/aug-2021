90d723eb9f700ee57f1516645dad5a79915e2c4d
Jagat Pradhan <jagat.pradhan0112@gmail.com>
2021-08-13 22:45:29 +0545

    min, value attribute of date and time is set on real time date and time

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,10 +2,17 @@ import { addTodos, markComplete } from "./modules/actions.js";
 import { deleteTodo } from "./modules/store.js";
 import renderItems from "./modules/renderItems.js";
 
+const date = document.getElementById("date");
+const time = document.getElementById("time");
 const todoInput = document.getElementById("todo-input");
 const addTodo = document.querySelector(".addTodo");
 const ul = document.querySelector("ul");
 const clearAll = document.querySelector(".clear-all");
+
+date.setAttribute("min", moment().format("YYYY-MM-DD"));
+date.setAttribute("value", moment().format("YYYY-MM-DD"));
+
+time.setAttribute("value", moment().format("hh:mm"));
 
 let todo = "";
 
@@ -29,5 +36,6 @@ ul.addEventListener("click", (e) => {
 
 // Clear All todos
 clearAll.addEventListener("click", () => {
-  ul.innerHTML = "";
+  localStorage.removeItem("todos");
+  renderItems();
 });
